897b6495b9b697ce2746fe6391af1521bfc130a7
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-12-27 20:25:50 +0530

    feat: created app listener

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,15 @@
-console.log("Working")
+import { log } from "console";
+import express from "express";
+
+const app = express()
+
+const PORT: number = parseInt(process.env.PORT, 10) || 4000
+const start = async () => {
+  try {
+    app.listen(PORT, () => log(`Server is listening on port ${PORT}...`))
+  } catch (error) {
+    log(error)
+  }
+}
+
+start()

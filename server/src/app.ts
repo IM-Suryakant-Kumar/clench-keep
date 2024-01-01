bc5cc6aa52f02435de382f507872ee1258c0115b
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-01-01 19:55:52 +0530

    feat: added notFound middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,30 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import notFoundMiddleware from "./middlewares/not-found";
 
-const app = express()
+const app = express();
 
-const PORT: number = parseInt(process.env.PORT, 10) || 4000
+// constant
+const PORT: number = parseInt(process.env.PORT, 10) || 4000;
+const CLIENT_URL: string = process.env.CLIENT_URL;
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
+
+app.use(notFoundMiddleware)
 const start = async () => {
-  try {
-    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`))
-  } catch (error) {
-    console.error(error)
-  }
-}
+	try {
+		app.listen(PORT, () =>
+			console.log(`Server is listening on port ${PORT}...`)
+		);
+	} catch (error) {
+		console.error(error);
+	}
+};
 
-start()
+start();
--- server/src/errors/index.ts
+++ server/src/errors/index.ts
@@ -1,37 +1,37 @@
 class CustomAPIError extends Error {
 	constructor(message: string) {
-		super(message)
+		super(message);
 	}
 }
 
 export class BadRequestError extends CustomAPIError {
-	statusCode: number
+	statusCode: number;
 	constructor(message: string) {
-		super(message)
-		this.statusCode = 400
+		super(message);
+		this.statusCode = 400;
 	}
 }
 
 export class NotFoundError extends CustomAPIError {
-	statusCode: number
+	statusCode: number;
 	constructor(message: string) {
-		super(message)
-		this.statusCode = 404
+		super(message);
+		this.statusCode = 404;
 	}
 }
 
 export class UnauthenticatedError extends CustomAPIError {
-	statusCode: number
+	statusCode: number;
 	constructor(message: string) {
-		super(message)
-		this.statusCode = 401
+		super(message);
+		this.statusCode = 401;
 	}
 }
 
 export class UnauthorizedError extends CustomAPIError {
-	statusCode: number
+	statusCode: number;
 	constructor(message: string) {
-		super(message)
-		this.statusCode = 403
+		super(message);
+		this.statusCode = 403;
 	}
 }

7674670d1e8dc83741a62243ff24599569c182e0
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-02-02 20:14:56 +0530

    feat: added trash apiSlice

--- client/src/features/apis/trash.ts
+++ client/src/features/apis/trash.ts
@@ -1,4 +1,5 @@
-import { ITrash, Response } from "../../types";
+import { toast } from "react-toastify";
+import { ErrorResponse, ITrash, Response } from "../../types";
 import { getTokenFromLocalStorage } from "../../utils";
 import api from "../api";
 
@@ -13,10 +14,66 @@ const trash = api.injectEndpoints({
 			providesTags: result =>
 				result ? [{ type: "trash", id: "LIST" }] : ["trash"],
 		}),
-    createTrash: build.mutation<Response, ITrash>({}),
-    updateTrash: build.mutation<Response, ITrash>({}),
-    deleteTrash: build.mutation<Response, ITrash>({})
+		createTrash: build.mutation<Response, ITrash>({
+			query: body => ({
+				url: "/archive",
+				method: "POST",
+				body,
+				headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` },
+			}),
+			invalidatesTags: (result, error) => {
+				if (result) {
+					toast.success(result.message);
+				} else {
+					const errorMessage = (error as ErrorResponse).data.message;
+					toast.error(errorMessage);
+				}
+				return result ? ["trash"] : [{ type: "trash", id: "ERROR" }];
+			},
+		}),
+		updateTrash: build.mutation<Response, ITrash>({
+			query: body => ({
+				url: `/trash/${body.noteId}`,
+				method: "PATCH",
+				body,
+				headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` },
+			}),
+			invalidatesTags: (result, error) => {
+				if (result) {
+					toast.success(result.message);
+				} else {
+					const errorMessage = (error as ErrorResponse).data.message;
+					toast.error(errorMessage);
+				}
+				return result
+					? [{ type: "trash", id: "LIST" }]
+					: [{ type: "trash", id: "ERROR" }];
+			},
+		}),
+		deleteTrash: build.mutation<Response, ITrash>({
+			query: body => ({
+				url: `/trash/${body.noteId}`,
+				method: "DELETE",
+				headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` },
+			}),
+			invalidatesTags: (result, error) => {
+				if (result) {
+					toast.success(result.message);
+				} else {
+					const errorMessage = (error as ErrorResponse).data.message;
+					toast.error(errorMessage);
+				}
+				return result
+					? [{ type: "trash", id: "LIST" }]
+					: [{ type: "trash", id: "ERROR" }];
+			},
+		}),
 	}),
 });
 
-export const {} = trash;
+export const {
+	useGetTrashesQuery,
+	useCreateTrashMutation,
+	useUpdateTrashMutation,
+	useDeleteTrashMutation,
+} = trash;

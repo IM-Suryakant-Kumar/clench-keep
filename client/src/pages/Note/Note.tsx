4346d490ee5625efd904c0695931c7cdb9ddb54f
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-02-11 18:53:39 +0530

    feat: empty note implemention

--- client/src/pages/Note/Note.tsx
+++ client/src/pages/Note/Note.tsx
@@ -1,10 +1,22 @@
+import styles from "./note.module.css";
 import { useGetNotesQuery } from "../../features/apis";
+import { INote } from "../../types";
 
 const Note = () => {
 	const { data } = useGetNotesQuery();
-	console.log(data?.notes);
+	const notes = data?.notes as INote[];
+	console.log(notes);
 
-	return <div>Note</div>;
+	return notes?.length > 0 ? (
+		<div>Note</div>
+	) : (
+		<div className={styles.emptyCont}>
+			<p className={styles.emptyDesc}>
+				You don't have any note yet. Create One
+			</p>
+			<button className={styles.emptyBtn}>CREATE NOTE</button>
+		</div>
+	);
 };
 
 export default Note;

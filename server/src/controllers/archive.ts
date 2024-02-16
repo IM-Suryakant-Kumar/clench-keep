ce964c5a39e4d94557ebd3755be1a30e57e0c726
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-02-16 14:51:39 +0530

    feat: updated server

--- server/src/controllers/archive.ts
+++ server/src/controllers/archive.ts
@@ -3,11 +3,17 @@ import { INoteReq } from "index";
 import { Note } from "../models";
 
 export const createArchive = async (req: INoteReq, res: Response) => {
-	await Note.findByIdAndUpdate(req.params._id, { isArchived: true });
+	await Note.findOneAndUpdate(
+		{ _id: req.params._id, userId: req.user },
+		{ isArchived: true }
+	);
 	res.status(201).json({ success: true, message: "Note Added to archive" });
 };
 
 export const deleteArchive = async (req: INoteReq, res: Response) => {
-	await Note.findByIdAndUpdate(req.params._id, { isArchived: false });
+	await Note.findOneAndUpdate(
+		{ _id: req.params._id, userId: req.user },
+		{ isArchived: false }
+	);
 	res.status(200).json({ success: true, message: "Note removed from Archive" });
 };
--- server/src/controllers/trash.ts
+++ server/src/controllers/trash.ts
@@ -3,11 +3,11 @@ import { INoteReq } from "index";
 import { Note } from "../models";
 
 export const createTrash = async (req: INoteReq, res: Response) => {
-	await Note.findByIdAndUpdate(req.params._id, { isTrashed: true });
+	await Note.findOneAndUpdate({_id: req.params._id, userId: req.user}, { isTrashed: true });
 	res.status(201).json({ success: true, message: "Note Added to trash" });
 };
 
 export const deleteTrash = async (req: INoteReq, res: Response) => {
-	await Note.findByIdAndUpdate(req.params._id, { isTrashed: false });
+	await Note.findOneAndUpdate({_id: req.params._id, userId: req.user}, { isTrashed: false });
 	res.status(200).json({ success: true, message: "Note removed from trash" });
 };
